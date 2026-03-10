2f5010b458151275e2b2ded4c7e5b392353fdef8
quantum-corn <akashkhetan044@gmail.com>
2026-03-10 20:22:09 +0530

    DEV: Add logic for the middleware validate.ts

--- backend/app/middlewares/validate.ts
+++ backend/app/middlewares/validate.ts
@@ -1,8 +1,13 @@
 import type { Request, Response, NextFunction } from "express";
+import { InvalidData } from "@whisper/shared/errors";
 import type { ZodObject } from "zod";
 
 export default function validate(Schema: ZodObject) {
   return (req: Request, res: Response, next: NextFunction) => {
-    // TODO: write logic
+    const formData = req.body;
+    const validatedFields = Schema.safeParse(formData);
+    if (!validatedFields.success) throw InvalidData;
+    else req.body = validatedFields.data;
+    next();
   };
 }
